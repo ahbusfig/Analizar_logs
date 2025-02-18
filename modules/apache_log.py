7e2106e956c7bacd1059b1843ce96935e1849e60
ahbusfig <ahbusfig@teleco.upv.es>
2025-02-18 19:46:25 +0100

    v1.1

--- modules/apache_log.py
+++ modules/apache_log.py
@@ -3,16 +3,7 @@ import re
 from termcolor import colored
 
 def parse_apache(filepath):
-    """
-    The regular expression pattern used for parsing log lines is:
-        r'(\d+\.\d+\.\d+\.\d+) - - \[(.*?)\] "(.*?)" (\d+) (\d+|-)'
-        - (\d+\.\d+\.\d+\.\d+): Matches the IP address.
-        - - -: Matches the literal characters " - - ".
-        - \[(.*?)\]: Matches the timestamp enclosed in square brackets.
-        - "(.*?)": Matches the request line enclosed in double quotes.
-        - (\d+): Matches the status code.
-        - (\d+|-) : Matches the size of the response in bytes or a dash if the size is not available.
-    """
+
     # Ejemplo pattern --> 192.168.0.2 - - [04/Feb/2025:10:06:47 -0500] "POST /login HTTP/1.1" 404 567
     pattern = r'(\d+\.\d+\.\d+\.\d+) - - \[(.*?)\] "(.*?)" (\d+) (\d+|-)'
     data = []
